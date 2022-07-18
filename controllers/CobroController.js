b239b4be93a72d30bd62824856e67380c2c01d88
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-07-18 15:57:34 -0500

    Commit funcion buscar cobros por tipo

--- controllers/CobroController.js
+++ controllers/CobroController.js
@@ -2,13 +2,29 @@ const express = require('express');
 const paymentController = express();
 const {cobroModel} = require('../models/CobroModel');
 const {JWTokenVerification} = require('../middleware/Authentication');
+const { QueryTypes } = require('@sequelize/core');
 
 // obtener todos los tipos de cobros
-paymentController.get('/getPaymentTypes',/**[JWTokenVerification],*/ (req, res) => {
+paymentController.get('/getPaymentTypes',[JWTokenVerification], (req, res) => {
     cobroModel.sequelize.query('CALL get_payment_types()').then((result) =>{
         return res.status(200).json({ok: true, result: result});
     }).catch((err) => {
-        return res.status(200).json({ok: false, message: 'Error al conectarse a la base de datos', err: err});
+        return res.status(400).json({ok: false, message: 'Error al conectarse a la base de datos', err: err});
+    })
+    
+});
+
+// obtener todos los cobros asociados a un tipo
+paymentController.get('/getPaymentByType/:paymentType',[JWTokenVerification], (req, res) => {
+    const query = 'CALL get_payments_by_payment_type(:tipo_cobro)';
+    cobroModel.sequelize.query(
+        query,
+        {type: QueryTypes.select,
+        replacements:{tipo_cobro: req.params.paymentType}
+        }).then((result) =>{
+        return res.status(200).json({ok: true, result: result});
+    }).catch((err) => {
+        return res.status(400).json({ok: false, message: 'Error al conectarse a la base de datos', err: err});
     })
     
 });
