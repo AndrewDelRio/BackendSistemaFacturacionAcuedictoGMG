9445cd00fd73a06449df05db12ea086a53a4befc
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-07-19 15:23:00 -0500

    Adicion de token a la funcion de facturacion

--- controllers/BillingController.js
+++ controllers/BillingController.js
@@ -5,7 +5,7 @@ const {JWTokenVerification} = require('../middleware/Authentication');
 const { QueryTypes } = require('@sequelize/core');
 
 //obtener facturas del ultimo periodo
-billingController.get('/getInvoiceLastPeriod',/**[JWTokenVerification],*/ (req, res) => {
+billingController.get('/getInvoiceLastPeriod',[JWTokenVerification], (req, res) => {
     facturaModel.sequelize.query('CALL get_invoice_by_billing_period(get_last_billing_period())').then((result) =>{
         return res.status(200).json({ok: true, result: result});
     }).catch((err) => {
