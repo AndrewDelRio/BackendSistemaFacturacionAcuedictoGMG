b5a486b1781097e7954024c1c5a0555899e46a8c
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-07-19 22:46:54 -0500

    Commit funcion obtener facturas que se pagaron

--- controllers/BillingController.js
+++ controllers/BillingController.js
@@ -14,7 +14,7 @@ billingController.get('/getInvoiceLastPeriod',[JWTokenVerification], (req, res) 
     
 });
 //recibir facturas que se pagaron
-billingController.post('/getUnpaidInvoices',[JWTokenVerification],(req, res) =>{
+billingController.post('/getpaidInvoices',[JWTokenVerification],(req, res) =>{
     const facturas = req.body.payment_list;
     if (facturas.length != 0) {
         const query = 'CALL update_invoice(:id_factura)';
