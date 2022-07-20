e5ddfa1368e83ef57fe1f205bdc42336693bcc54
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-07-19 22:02:33 -0500

    Commit funcion actualizar facturas canceladas

--- controllers/BillingController.js
+++ controllers/BillingController.js
@@ -13,6 +13,24 @@ billingController.get('/getInvoiceLastPeriod',[JWTokenVerification], (req, res) 
     })
     
 });
+//recibir facturas que no se pagaron
+billingController.post('/getUnpaidInvoices',[JWTokenVerification],(req, res) =>{
+    const facturas = req.body.id_factura;
+    if (facturas.length != 0) {
+        const query = 'CALL update_invoice(:id_factura)';
+        const facturaId = facturas.map(function(factura) { 
+        facturaModel.sequelize.query(query,
+            {type: QueryTypes.UPDATE,
+                replacements:{id_factura: factura}
+            }).then((result) =>{
+                return res.status(200).json({ok:true, result:result[0],message: 'Registros actualizados'});
+            })
+        })
+    }else{
+        return res.status(400).json({ok: false, message: 'No se encontraron registros para actualizar'});
+    }
+    
+})
 
 module.exports = {billingController};
 
