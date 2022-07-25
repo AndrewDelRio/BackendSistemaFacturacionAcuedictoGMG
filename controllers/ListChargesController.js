b0d180c407f43a0474fccebaf8ad3951a9ea89a2
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-07-25 09:35:29 -0500

    Commit funcion agregar cobros a las matriculas

--- controllers/ListChargesController.js
+++ controllers/ListChargesController.js
@@ -6,17 +6,26 @@ const { QueryTypes } = require('@sequelize/core');
 
 // agregar las sanciones a las matriculas
 listChargesController.post('/registerChargesList',[JWTokenVerification], (req, res) => {
-    const cobrosList = req.body.charge_list;
-    console.log(cobrosList.length);
-    const query = 'CALL register_charges_and_penalties(:id_cobro,:id_matricula,:fecha_cobro,:cantidad_unidades,:observaciones)';
-        listaDeCobroModel.sequelize.query(query,
-            {type: QueryTypes.INSERT,
-                replacements:{id_cobro:cobrosList.id_payment, id_matricula:cobrosList.id_enrollment,fecha_cobro:new Date(),cantidad_unidades: cobrosList.quantity, observaciones: cobrosList.observations}
-            }).then((result) =>{
-                    return res.status(200).json({ok:true, message: 'Cobros registrados exitosamente'});
-            }).catch((err) => {
-                res.status(400).json({ok: false, err:err, message: 'Error al conectarse a la base de datos'});
-            })
+    const cobrosList = req.body.charges;
+    if (cobrosList.length != 0) {
+        let counterUpdatesCharges = 0;
+        const query = 'CALL register_charges_and_penalties(:id_cobro,:id_matricula,:fecha_cobro,:cantidad_unidades,:observaciones)';
+        cobrosList.map(function(cobro) { 
+            listaDeCobroModel.sequelize.query(query,
+                {type: QueryTypes.INSERT,
+                    replacements:{id_cobro:cobro.id_payment, id_matricula:cobro.id_enrollment,fecha_cobro:new Date(),cantidad_unidades: cobro.quantity, observaciones: cobro.observations}
+                }).then((result) =>{
+                    counterUpdatesCharges++;
+                    if (counterUpdatesCharges === cobrosList.length) {
+                        return res.status(200).json({ok:true, message: 'Cobros registrados exitosamente'});
+                    }
+                }).catch((err) => {
+                    return res.status(400).json({ok: false, err:err, message: 'Error al conectarse a la base de datos'});
+                })
+        })
+    }else{
+        return res.status(200).json({ok: true, message: 'No existen registros para actualizar'});
+    }
 });
 
 module.exports = {listChargesController};
