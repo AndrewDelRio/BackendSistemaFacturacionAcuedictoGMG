3026e70dba4c0b8a11fbaf29e11988945f7dee79
Andres Del Rio <andres.simbaqueba@uptc.edu.co>
2022-11-12 11:02:59 -0500

    Cambios en los datos de conexion de la DB

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-const dbUserName = 'u88cwzub9lqvg88i';
-const dbPassword = 'WZB5sdt8w1gRxUcH4MWM';
-const dbName = 'bpidcmf0rywnnilobkjx';
-const dBHost = 'bpidcmf0rywnnilobkjx-mysql.services.clever-cloud.com';
-const dBPort = '20183';
+const dbUserName = 'ufz1wimlrdorjkgn';
+const dbPassword = 'fxbDx44LeLmpJW9qf0rq';
+const dbName = 'be3ecb62hjw6d2ksplo1';
+const dBHost = 'be3ecb62hjw6d2ksplo1-mysql.services.clever-cloud.com';
+const dBPort = '3306';
 
 process.env.JWT_SIGNATURE = process.env.JWT_SIGNATURE || 'developmenttokensignature';
 process.env.JWT_EXP_TIME = process.env.JWT_EXP_TIME || '10h';
